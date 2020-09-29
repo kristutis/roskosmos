48dbbadf379ef2b7052c61d8cfabbeac4864b7b5
kristutis <kristijonas.karalaitis@gmail.com>
2020-09-29 15:20:39 +0300

    added push script

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+if [[ $# != 1 ]]
+then
+	echo "Naudojimas: ./push.sh \"commit_message\""
+	echo "Pvz:"
+	echo "./push.sh \"prideta nauja funkcija\""
+	exit 1
+fi
+
+git add .
+commit_message=$1
+git commit -m "$commit_message"
+git push
+
+# cancellint commitus:
+# git reset HEAD~1
