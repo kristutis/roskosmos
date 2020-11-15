a6c51494aa7d464cbdc394b390321bd01ca8245f
kristutis <kristijonas.karalaitis@gmail.com>
2020-11-15 20:18:02 +0200

    backup front

--- web/react-front/src/components/NavBar.js
+++ web/react-front/src/components/NavBar.js
@@ -4,6 +4,29 @@ import { Link } from 'react-router-dom'
 import logo from './logo.jpg'
 
 function NavBar() {
+    const role = "klientas"
+
+    if (role=="klientas") {
+        return (
+            <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">                    
+                        <img className="rounded-circle" src={logo} width="40" height="40"/>
+                        ROSKOSMOS
+                    </Link>
+                    <Link to="/profile" className="navbar-logo">                    
+                        <img className="rounded-circle" src={logo} width="40" height="40"/>
+                        ROSKOSMOS
+                    </Link>
+                </div>      
+            </nav>
+    
+            
+            </>
+        )
+    }
+
     return (
         <>
         <nav className="navbar">
@@ -16,8 +39,9 @@ function NavBar() {
                     <img className="rounded-circle" src={logo} width="40" height="40"/>
                     ROSKOSMOS
                 </Link>
+                <button>prisijungti</button>
             </div>      
-        </nav>
+        </nav>        
         </>
     )
 }
