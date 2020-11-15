ef01fb465a140f992dae364951aa14b437236724
kristutis <kristijonas.karalaitis@gmail.com>
2020-11-15 20:21:32 +0200

    navbar

--- web/react-front/src/App.js
+++ web/react-front/src/App.js
@@ -1,4 +1,4 @@
-// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
--- web/react-front/src/components/NavBar.js
+++ web/react-front/src/components/NavBar.js
@@ -4,28 +4,8 @@ import { Link } from 'react-router-dom'
 import logo from './logo.jpg'
 
 function NavBar() {
-    const role = "klientas"
+    const role = "asd"
 
-    if (role=="klientas") {
-        return (
-            <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">                    
-                        <img className="rounded-circle" src={logo} width="40" height="40"/>
-                        ROSKOSMOS
-                    </Link>
-                    <Link to="/profile" className="navbar-logo">                    
-                        <img className="rounded-circle" src={logo} width="40" height="40"/>
-                        ROSKOSMOS
-                    </Link>
-                </div>      
-            </nav>
-    
-            
-            </>
-        )
-    }
 
     return (
         <>
@@ -39,7 +19,14 @@ function NavBar() {
                     <img className="rounded-circle" src={logo} width="40" height="40"/>
                     ROSKOSMOS
                 </Link>
-                <button>prisijungti</button>
+                
+                {role=="klientas" ? 
+                <div>
+                    <button>prisijungti</button>
+                </div> : 
+                <div>
+                    neprisijunges
+                </div>}
             </div>      
         </nav>        
         </>
