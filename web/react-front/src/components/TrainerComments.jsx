914749b7ae0cf874085e21b0410821185d03599b
kristutis <kristijonas.karalaitis@gmail.com>
2020-11-17 01:49:56 +0200

    added treneris page

---
+++ web/react-front/src/components/TrainerComments.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const source="https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70"
+
+export default function TrainerComments() {
+    return (
+        <div>
+            <table class="table table-striped table-dark">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">paveiks</th>
+                    <th scope="col">Vardas</th>
+                    <th scope="col">Komentaras</th>
+                    <th scope="col">Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td><img class="rounded-circle img-thumbnail" src="https://randomuser.me/api/portraits/women/72.jpg" width="70" height="70"/></td>
+                        <td>Akvilė</td>
+                        <td>“Nėra nieko, kas tave stabdytų, išskyrus tave patį”</td>
+                        <td>2020-20-20</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td><img class="rounded-circle img-thumbnail" src="https://randomuser.me/api/portraits/men/61.jpg" width="70" height="70"/></td>
+                        <td>Dovydas</td>
+                        <td>“Juk taip smagu daryti tai, kas neįmanoma!”</td>
+                        <td>2020-20-20</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td><img class="rounded-circle img-thumbnail" src="https://randomuser.me/api/portraits/women/59.jpg" width="70" height="70"/></td>
+                        <td>Raminta</td>
+                        <td>“Gyvenime nėra nieko neįmanoma... Vienintelis klausimas tik, ar užteks drąsos siekti tai, ko iš tiesų nori?”</td>
+                        <td>2020-20-20</td>
+                    </tr>                     
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- web/react-front/src/pages/Treneris.jsx
+++ web/react-front/src/pages/Treneris.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import TrainerComments from '../components/TrainerComments'
 import './Treneris.css'
+
+
 
 export default function Treneris() {
     return (
@@ -10,41 +13,8 @@ export default function Treneris() {
                     <div className="col-md-6">                        
                         <img width="100%" height="700" src="https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70"/>
                     </div>
-                    <div className="col-md-6">                        
-                        <table class="table table-striped table-dark">
-                            <thead>
-                                <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">paveiks</th>
-                                <th scope="col">Vardas</th>
-                                <th scope="col">Frazė</th>
-                                <th scope="col">Plačiau</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td><img class="img-thumbnail" src="https://randomuser.me/api/portraits/women/72.jpg" width="70" height="70"/></td>
-                                    <td>Akvilė</td>
-                                    <td>“Nėra nieko, kas tave stabdytų, išskyrus tave patį”</td>
-                                    <td><button type="button" class="btn btn-secondary">Plačiau</button></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td><img class="img-thumbnail" src="https://randomuser.me/api/portraits/men/61.jpg" width="70" height="70"/></td>
-                                    <td>Dovydas</td>
-                                    <td>“Juk taip smagu daryti tai, kas neįmanoma!”</td>
-                                    <td><button type="button" class="btn btn-secondary">Plačiau</button></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td><img class="img-thumbnail" src="https://randomuser.me/api/portraits/women/59.jpg" width="70" height="70"/></td>
-                                    <td>Raminta</td>
-                                    <td>“Gyvenime nėra nieko neįmanoma... Vienintelis klausimas tik, ar užteks drąsos siekti tai, ko iš tiesų nori?”</td>
-                                    <td><button type="button" class="btn btn-secondary">Plačiau</button></td>
-                                </tr>                     
-                            </tbody>
-                        </table>
+                    <div className="col-md-6">        
+                        <TrainerComments/>
                         <h1>Ivertinimas</h1>
                         <br></br>
                         <h4>komentaras</h4>
