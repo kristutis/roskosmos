87f639904b05c0b6b9f46dedb98159655658ff3c
kristutis <kristijonas.karalaitis@gmail.com>
2020-11-18 20:17:17 +0200

    done login modal

--- web/react-front/src/components/LoginModal.jsx
+++ web/react-front/src/components/LoginModal.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Modal from 'react-modal';
+import './LoginModal.css'
 
 export default function LoginModal(props) {
     return (        
@@ -7,39 +8,38 @@ export default function LoginModal(props) {
         centered isOpen={props.isOpen} onRequestClose={() => props.toClose(false)}
             style={
                 {
+                    content: {
+                        // overflow: 'hidden',
+                        top: '30%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        width: '30%',
+                        height: '40%',
+                        transform: 'translate(-40%, -10%)',
+                        background: 'rgba(255,255,255, 0.7)',      
+                      },
                     overlay: {
-                        backgroundColor: 'grey',
+                        background: 'rgba(0,0,0, 0.8)',          
                     },
-                    size: {
-                        
-                    }
                 }                
             }>
-            {/* <Modal.Header className="login-header">
-                <Modal.Title className="text-bold text-uppercase">Prisijungimas</Modal.Title>
-                <i className="close-button fas fa-times" alt="foto"/>
-            </Modal.Header> */}
-            <form>
-
-                    <label>El. Paštas</label>
-                    <input type="email" className="border-zero submit-text" placeholder=""  />
-
-
-                    <label>Slaptažodis</label>
-                    <input type="password" className="border-zero submit-text text-bold" placeholder="" />
-
-                {/* <Row>
-                    <Col sm="8" className="pt-3"> */}
-                        <a href="#"  ><p className="mb-0">Pamiršote slaptažodį?</p></a>
-                    {/* </Col> */}
-                    {/* <Col sm="4"> */}
-                        <button className="float-right btn-loginsubmit text-bold text-uppercase" variant="link" type="button" >Prisijungti</button>
-                    {/* </Col> */}
-                {/* </Row> */}
-            </form> 
-            <h1>hello</h1>
-            <p>ppl</p>
-            <button onClick={() => props.toClose(false)}></button>
+                <div onClick={() => props.toClose(false)} className="close-x">+</div>
+                <br></br>
+                <br></br>
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input required type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                        <small id="emailHelp" class="form-text text-muted">*We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input required type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <input type="submit" class="btn btn-primary btn-block btn-lg" value="Login"/>
+                </form>
         </Modal >
     )
 }
